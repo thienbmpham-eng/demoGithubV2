b9c688b06a9d2a0bdc8e22ea469d5fed602dca3e
ThangBM <buithang15082005@gmail.com>
2026-05-15 17:23:56 +0700

    Thang
    Thang update Student class

---
+++ Q1_GEMINI/src/java/controller/Student.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Student {
+    private String studentID;
+    private int age;
+    private String gender;
+
+    public Student(String studentID, int age, String gender) {
+        this.studentID = studentID;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    
+    
+}
